44fdaa58396d35e65c914b5b92ac8eff3372c01a
AnastasiaDDM <ndmitrievnaa@mail.ru>
2020-03-14 14:01:08 +0800

    14.03.2020 14:01:08,43

--- фомочка статьи/js/javascript.js
+++ фомочка статьи/js/javascript.js
@@ -71,6 +71,11 @@ let inputs = document.querySelectorAll('.input__file');
               })
   
 
+  
+  $('.datepicker').datepicker({
+    language: 'en',
+    minDate: new Date() // Now can select only dates, which goes after today
+})
 
 
        
