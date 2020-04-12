36fe1ab3d69a8ec8eebf028134b4b6157abc998d
AnastasiaDDM <ndmitrievnaa@mail.ru>
2020-04-13 03:59:42 +0800

    13.04.2020  3:59:42,16

--- Сайт/js/javascript.js
+++ Сайт/js/javascript.js
@@ -1,71 +1,54 @@
   
-	  function select() {
+
+// Ф-ия для стилизации селекта (выпадающего списка) 
+function select() {
   $('.slct').click(function(){
-	/* Заносим выпадающий список в переменную */
-	var dropBlock = $(this).parent().find('.drop');
+		/* Заносим выпадающий список в переменную */
+		var dropBlock = $(this).parent().find('.drop');
 
-	/* Делаем проверку: Если выпадающий блок скрыт то делаем его видимым*/
-	if( dropBlock.is(':hidden') ) {
-		dropBlock.slideDown();
+		/* Делаем проверку: Если выпадающий блок скрыт то делаем его видимым*/
+		if( dropBlock.is(':hidden') ) {
+			dropBlock.slideDown();
 
-		/* Выделяем ссылку открывающую select */
-		$(this).addClass('active');
+			/* Выделяем ссылку открывающую select */
+			$(this).addClass('active');
 
-		/* Работаем с событием клика по элементам выпадающего списка */
-		$('.drop').find('li').click(function(){
+			/* Работаем с событием клика по элементам выпадающего списка */
+			$('.drop').find('li').click(function(){
 
-			/* Заносим в переменную HTML код элемента
-			списка по которому кликнули */
-			var selectResult = $(this).find('a').data('value');
-			var selectResultName = $(this).find('a').html();
+				/* Заносим в переменную HTML код элемента
+				списка по которому кликнули */
+				var selectResult = $(this).find('a').data('value');
+				var selectResultName = $(this).find('a').html();
 
-			/* Находим наш скрытый инпут и передаем в него
-			значение из переменной selectResult */
-			$(this).parent().parent().find('input').val(selectResult);
+				/* Находим наш скрытый инпут и передаем в него
+				значение из переменной selectResult */
+				$(this).parent().parent().find('input').val(selectResult).trigger('change');
 
-			/* Передаем значение переменной selectResult в ссылку которая
-			открывает наш выпадающий список и удаляем активность */
-			$(this).parent().parent().find('.slct').removeClass('active').html(selectResultName);
+				/* Передаем значение переменной selectResult в ссылку которая
+				открывает наш выпадающий список и удаляем активность */
+				$(this).parent().parent().find('.slct').removeClass('active').html(selectResultName);
 
-			/* Скрываем выпадающий блок */
+				/* Скрываем выпадающий блок */
+				dropBlock.slideUp();
+			});
+
+		/* Продолжаем проверку: Если выпадающий блок не скрыт то скрываем его */
+		} 
+		else {
+			$(this).removeClass('active');
 			dropBlock.slideUp();
-		});
+		}
 
-	/* Продолжаем проверку: Если выпадающий блок не скрыт то скрываем его */
-	} else {
-		$(this).removeClass('active');
-		dropBlock.slideUp();
-	}
-
-	/* Предотвращаем обычное поведение ссылки при клике */
-	return false;
-});
-
+		/* Предотвращаем обычное поведение ссылки при клике */
+		return false;
+	});
 }
 
 
- 
- function input_file() {
-let inputs = document.querySelectorAll('.input__file');
-              Array.prototype.forEach.call(inputs, function (input) {
-                let label = input.nextElementSibling,
-                  labelVal = label.querySelector('.input__file-button-text').innerText;
-           
-                input.addEventListener('change', function (e) {
-                  let countFiles = '';
-                  if (this.files && this.files.length >= 1)
-                    countFiles = this.files.length;
-           
-                  if (countFiles)
-                    label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
-                  else
-                    label.querySelector('.input__file-button-text').innerText = labelVal;
-                });
-              });
- }
 
-
- function search_roll() {
+// Ф-ия сворачивания и разворота поиска
+function search_roll() {
 
 	if ($( window ).width() < 600) {
 			$( ".search_roll_up" ).hide();
@@ -99,9 +82,8 @@ let inputs = document.querySelectorAll('.input__file');
 }
 
 
- 
- function input_filled()
-{
+// Ф-ия корректировки работы input (для label)
+function input_filled() {
   $('.input_style input').each(function(){
     $(this).change(function(){
       if ($(this).val().length > 0)
@@ -116,8 +98,8 @@ let inputs = document.querySelectorAll('.input__file');
   });
 }
  
- 
-  function textarea_filled()
+// Ф-ия корректировки работы textarea (для label)
+function textarea_filled()
 {
   $('textarea').each(function(){
     $(this).change(function(){
@@ -135,7 +117,7 @@ let inputs = document.querySelectorAll('.input__file');
  
  
 
-
+// Ф-ия раскрытия списка всех возможных областей (поиск в каталоге)
 function areas_more() {
 	$( ".areas_more" ).click(function() {
 
@@ -150,92 +132,111 @@ function areas_more() {
 	  });
 
 	});
-  }
+}
  
 
-  function activate_form_answer()
-  {
+// Ф-ия сворачивания формы ответа и отображение кнопки ответа
+function comments_form_answer_hide() {
 	$(".btn_cancel").click(function() {
-		$(this).parents(".copy_form_answer").hide(500);
+
+		// Скрытие текущей формы ответа
+		$(this).parents(".copy_form_answer").hide(500 , function () { 
+			$(this).parents(".copy_form_answer").detach();
+		});
+
+		// Показ всех кнопок Ответить
 		$(".btn_answer").show(500);
 		$(".btn_answer_two").show(500);
 	});
-  }
+}
 
 
 
 
 
 
-
-  function comments_form_answer()
-  {
+// Ф-ия прорисовки формы ответа в обсуждениях и показ комментариев
+function comments_form_answer() {
 
 	$(".btn_answer" ).click( function () {
 
+		// Показ всех кнопок Ответить
 		$(".btn_answer").show(500);
+		
+		// Скрытие текущей нажатой кнопки Отвтеить 
 		$(this).hide(500);
-		$("[data-rel='single_form_answer']").hide(500);
-		$("[data-rel='single_form_answer']").detach();
+
+		// Скрытие всех форм ответа и уничтожение
+		$("[data-rel='single_form_answer']").hide(500, function () { 
+			$("[data-rel='single_form_answer']").detach();
+		});
   
+		// Копирование шаблона формы ответа
 		var form = $( "#pattern_form_answer").html();
   
+		// Вставка формы ответа
 		$(this).parent().append( $(form) );
   
-		// console.log($(this).siblings(".copy_form_answer"));
-  
+		// Добавление атрибута, по которому потом будет удаляться эта форма
 		$($(this).siblings(".copy_form_answer").attr( "data-rel", "single_form_answer" ));
   
+		// Показ формы
 		$("[data-rel='single_form_answer']").show(500);
   
-		activate_form_answer();
+		comments_form_answer_hide();
 	  });
   
 	  $( ".btn_answer_two" ).click(function () {
   
+	      // Показ всех кнопок Ответить
 		  $(".btn_answer_two").show(500);
+
+		  // Скрытие текущей нажатой кнопки Отвтеить 
 		  $(this).hide(500);
   
+		  // Скрытие всех форм ответа и уничтожение
 		  $("[data-rel='single_form_answer']").hide(500);
 		  $("[data-rel='single_form_answer']").detach();
   
-		  // var bt_data = $(this).data("target");
-  
+		  // Копирование шаблона формы ответа
 		  var form = $( "#pattern_form_answer").html(); 
   
+		  // Вставка формы ответа
 		  $(this).parents(".discussion").append( $(form) );
-  
+
+  		  // Добавление атрибута, по которому потом будет удаляться эта форма
 		  $($( ".discussion" ).children( ".copy_form_answer" ).attr( "data-rel", "single_form_answer" ));
   
+		  // Показ формы
 		  $("[data-rel='single_form_answer']").show(500);
   
-		  activate_form_answer();
+		  comments_form_answer_hide();
 	  });
   
+	  // Нажатие кнопки просмотра комментариев 
 	  $( ".comment_roll" ).click(function() {
   
 		var com_data = $(this).data("target");
   
-		$( "[id="+com_data+"] .discussion_comment" ).slideToggle( "slow", function() {
-  
-		});
+		// Разворот/сворачивание комментариев
+		$( "#"+com_data+" .discussion_comment" ).slideToggle( "slow" );
 	  });
   
+	  // Нажатие кнопки сворачивания комментариев 
 	  $( ".btn_rollup" ).click(function() {
 		
-		$( this ).parents(".discussion_comment").slideToggle( "slow", function() {
-		});
+		// Разворот/сворачивание комметнариев
+		$( this ).parents(".discussion_comment").slideToggle( "slow" );
 	  });
-
-  }
-
+}
 
 
 
 
 
 
-  function carousel_soft(target) {
+// Ф-ия управления виджетами, каруселями ПО
+function carousel_soft(target) {
 	$('#'+target).slick({
 		infinite: true,
 		slidesToShow: 3,
@@ -243,51 +244,43 @@ function areas_more() {
 		speed: 300,
 		autoplay: true,
 		autoplaySpeed: 2000,
-	respondTo: 'min',
-	
-	responsive: [
-	{
-	  breakpoint: 1024,
-	  settings: {
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		infinite: true,
-	  }
-	},
-	{
-	  breakpoint: 790,
-	  settings: {
-		slidesToShow: 2,
-		slidesToScroll: 1
-	  }
-	},
-	{
-	  breakpoint: 480,
-	  settings: {
-		slidesToShow: 1,
-		slidesToScroll: 1
-	  }
-	}
-	],
-	arrows:true,
-	appendArrows: $("#"+target+"_container"),
-	prevArrow:'<i class="fa fa-angle-left pos_center widget_arrows" aria-hidden="true" ></i>',
-	nextArrow:'<i class="fa fa-angle-right pos_center widget_arrows" aria-hidden="true"></i>',
-	
-		})
-  }
+		respondTo: 'min',
+		
+		responsive: [
+			{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+			},
+			{
+			breakpoint: 790,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+			}
+		],
+		arrows:true,
+		appendArrows: $("#"+target+"_container"),
+		prevArrow:'<i class="fa fa-angle-left pos_center widget_arrows" aria-hidden="true" ></i>',
+		nextArrow:'<i class="fa fa-angle-right pos_center widget_arrows" aria-hidden="true"></i>',
+		
+	})
+}
   
- 
- jQuery(document).ready(function () {
-	
-	select();
-	input_file();
-	input_filled();
-	textarea_filled();
-	search_roll();
-	areas_more();
-	comments_form_answer();
 
+// Ф-ия инициализации input для даты
+function datepicker_init() {
 	if ($( window ).width() < 600) {
 		$('.search_datepicker').each(function() {
 			$( this  ).attr( "type", "date" );
@@ -301,6 +294,16 @@ function areas_more() {
 				// minDate: new Date() // Now can select only dates, which goes after today
 			});
 	}
-		 
+}
 
-})
+
+//  jQuery(document).ready(function () {
+	
+// 	select();
+// 	input_filled();
+// 	textarea_filled();
+// 	search_roll();
+// 	areas_more();
+// 	comments_form_answer();
+// 	datepicker_init();
+// })
